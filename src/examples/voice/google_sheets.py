3ca3be6b495eb04504752a146e9b6e436e9907d8
Ivan Bogun <gnattuha@gmail.com>
2018-07-01 21:53:51 -0700

    Google spreadsheets MVP

--- src/examples/voice/google_sheets.py
+++ src/examples/voice/google_sheets.py
@@ -1,9 +1,13 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-SECRETS_FILE="../../../secrets/1-button-cardbox-78c4daa3a219.json"
-SPREADSHEET_NAME="Baby Elsa"
+import os
+fileDir = os.path.dirname(os.path.abspath(__file__)) 
+print(fileDir)
 
+SECRETS_FILE=os.path.join(fileDir, "secrets", "1-button-cardbox-78c4daa3a219.json")
+SPREADSHEET_NAME="Baby Elsa"
+print(SECRETS_FILE)
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(SECRETS_FILE, scope)
 gc = gspread.authorize(credentials)
